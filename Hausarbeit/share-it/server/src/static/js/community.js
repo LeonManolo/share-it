f290da930a306d730bffad6c68ed3ae671851b06
NiklasHargarter <niklashargarter@gmail.com>
2021-12-08 12:19:04 +0100

    community weiter gemacht

--- Hausarbeit/share-it/server/src/static/js/community.js
+++ Hausarbeit/share-it/server/src/static/js/community.js
@@ -1,3 +1,4 @@
+
 buildFriendRequestSection();
 
 async function buildFriendRequestSection() {
@@ -7,6 +8,30 @@ async function buildFriendRequestSection() {
 
   // Url an die die anfrage gemacht wird
   const url = "http://localhost:8080/open-friend-requests";
+
+  let response;
+  try {
+    response = await fetch(url);
+    const friendRequests = await response.json();
+    if (response.ok) {
+      friendRequests.forEach((friend) => {
+        friendRequestSection.appendChild(buildFriendRequestTile(friend));
+      });
+    } else {
+      console.log("Could not fetch open friend requests!");
+    }
+  } catch (e) {
+    console.log(`Netzwerk Fehler ${e}`);
+  }
+}
+
+async function buildFriendSection(){
+  const friendRequestSection = document.getElementById("friends");
+  const h2 = document.createElement("h2");
+  h2.textContent = "Freunde";
+
+  // Url an die die anfrage gemacht wird
+  const url = "http://localhost:8080/get-friends";
 
   let response;
   try {
@@ -28,17 +53,52 @@ function buildFriendTile(friend) {
   console.log(friend);
   const div = document.createElement("div");
   div.className = "friendTile"
-  //TODO: img src setzen und hinzufügen
+
   const img = document.createElement("img");
-  img.src = `${element.imageUrl}`;
-  img.alt = "Falsche URL";
-  div.textContent = friend.username;
+  img.src = `${friend.imageUrl}`;
+  img.alt = "Image Error";
+  div.appendChild(img)
+
+  var name = document.createElement('p');
+  var nameText = document.createTextNode(`${friend.username}`);
+  name.appendChild(nameText);
+  div.appendChild(name);
+
   return div;
 }
 
 function buildFriendRequestTile(user){
+  console.log(user);
   const div = document.createElement("div");
   div.className = "friendRequest"
-  //TODO: img src setzen und hinzufügen
+
   const img = document.createElement("img");
+  img.src = `${user.imageUrl}`;
+  img.alt = "Image Error";
+  div.appendChild(img)
+
+  var name = document.createElement('p');
+  var nameText = document.createTextNode(`${user.username}`);
+  name.appendChild(nameText);
+  div.appendChild(name);
+
+  var acceptButton = document.createElement('button');
+  acceptButton.innerHtml = "Akzeptieren";
+  acceptButton.onclick = function(){acceptFriendRequest(user.friendshipId)};
+  div.appendChild(acceptButton);
+
+  var declineButton = document.createElement('button');
+  declineButton.innerHtml = "Ablehnen";
+  declineButton.onclick = function(){declineFriendRequest(user.friendshipId)};
+  div.appendChild(declineButton);
+
+  return div;
+}
+
+function acceptFriendRequest(id){
+  console.log(id);
+}
+
+function declineFriendRequest(id){
+  console.log(id);
 }
